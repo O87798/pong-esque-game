2d869924b3e3c6a30af576dba56cb86b8997b081
lqsr_ <>
2021-04-21 20:04:36 +0000

    added abstract gameObj class

---
+++ gameObj.java
@@ -0,0 +1,29 @@
+public abstract class gameObj {
+
+	public int x = 0;
+	public int xa;
+	public int y = 0;
+	public int ya;
+	public int width = 30;
+	public int height = 30;
+	public GamePanel panel;
+	public Rectangle coll;
+
+	public gameObj(GamePanel panel) {
+		this.panel = panel;
+	}
+
+	public void move() {
+		coll.setBounds(x,y,width,height);
+		x = x + xa;
+		y = y + ya;
+	}
+
+	public void paint(Graphics g) {
+
+	}
+
+	public void initCollision() {
+		coll = new Rectangle(x,y,width,height);
+	}
+}
